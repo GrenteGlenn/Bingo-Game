81d6ce54bfa807de9098c232a26f6706cad07b94
GrenteGlenn <grenteglenn@gmail.com>
2026-01-06 12:31:00 +0100

    [Fix]token

--- cagnotte/lib/playerToken.ts
+++ cagnotte/lib/playerToken.ts
@@ -1,11 +1,11 @@
-export function getPlayerToken(): string {
+export function getPlayerToken() {
   if (typeof window === "undefined") return "";
 
-  let token = localStorage.getItem("bingo-token");
+  let token = sessionStorage.getItem("bingo-token");
 
   if (!token) {
     token = crypto.randomUUID();
-    localStorage.setItem("bingo-token", token);
+    sessionStorage.setItem("bingo-token", token);
   }
 
   return token;
